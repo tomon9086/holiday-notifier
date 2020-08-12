92d379b14c1e4861ec6bd660a0d5052686cc1e5a
tomon9086 <tomo.mouse.9086@gmail.com>
2020-08-12 18:30:07 +0900

    完成

--- flask/app/main.py
+++ flask/app/main.py
@@ -5,12 +5,9 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from datetime import datetime, timedelta
 import os
 import json
-import asyncio
-import schedule
-# import time
 
-from .modules.holiday import what_day_is, get_holidays_in_this_month
-from .modules.utils import key_to_date
+from modules.holiday import what_day_is, get_holidays_in_this_month
+from modules.utils import key_to_date
 
 app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ["CHANNEL_ACCESS_TOKEN"])
@@ -21,12 +18,36 @@ def today():
   return json.dumps(what_day_is(datetime.now()), ensure_ascii=False).encode("utf-8")
 
 
+@app.route('/_everyday')
+def everyday():
+  today = datetime.now()
+
+  if today.day == 1:
+    holidays = get_holidays_in_this_month(today)
+    text = str(today.month) + "月の祝日は"
+    if len(holidays) > 0:
+      for date, name in holidays:
+        text += "\n" + date.strftime("%m月%d日") + " の " + name
+      text += "\nです"
+    else:
+      text += "ありません"
+    line_bot_api.broadcast(TextSendMessage(text=text))
+    
+    holiday_name = what_day_is(today + timedelta(1))
+    text = "明日は"
+    if holiday_name is not None:
+      text += holiday_name + "です"
+      line_bot_api.broadcast(TextSendMessage(text=text))
+      
+  return "done"
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
   signature = request.headers['X-Line-Signature']
 
   body = request.get_data(as_text=True)
-  app.logger.info("Request body: " + body)
+  # app.logger.info("Request body: " + body)
 
   try:
     handler.handle(body, signature)
@@ -52,6 +73,7 @@ def handle_message(event):
       reply_text += holiday_name + "です"
     else:
       reply_text += "祝日ではありません"
+      
   elif "今月" in recieved_text:
     holidays = get_holidays_in_this_month(datetime.now())
     reply_text = "今月の祝日は"
@@ -66,45 +88,6 @@ def handle_message(event):
     event.reply_token,
     TextSendMessage(text=reply_text))
 
-
-def everyday():
-  app.logger.info("everyday")
-  today = datetime.now()
-
-  if today.day == 11:
-    holidays = get_holidays_in_this_month(today)
-    text = today.month + "月の祝日は"
-    if len(holidays) > 0:
-      for date, name in holidays:
-        text += "\n" + date.strftime("%m月%d日") + " の " + name
-      text += "\nです"
-    else:
-      text += "ありません"
-    line_bot_api.broadcast(TextSendMessage(text=text))
-    
-    holiday_name = what_day_is(today + timedelta(1))
-    text = "明日は"
-    if holiday_name is not None:
-      text += holiday_name + "です"
-    else:
-      text += "祝日ではありません"
-    line_bot_api.broadcast(TextSendMessage(text=text))
-
-
-async def watch_schedule():
-  while True:
-    schedule.run_pending()
-    app.logger.info("watching...")
-    await asyncio.sleep(1)
-
-# while True:
-#   schedule.run_pending()
-#   app.logger.info("watching...")
-#   time.sleep(1)
-
-schedule.every().day.at("9:00").do(everyday)
-# schedule.every().minute.do(lambda: print("run every minute"))
-asyncio.run(watch_schedule())
 
 if __name__ == '__main__':
   app.run()
