f25b2845bc2839e9820f7aae03e23385c1759d15
tomon9086 <tomon9086@gmail.com>
2021-03-08 00:56:17 +0900

    broadcast

---
+++ functions/src/holiday/index.ts
@@ -0,0 +1,50 @@
+import { compareDate, dateInMonth } from '@/utils'
+import { DateTime } from 'luxon'
+
+const url = 'https://holidays-jp.github.io/api/v1/date.json'
+
+type Holiday = {
+  date: Date
+  text: string
+}
+
+export const getHolidays = async (): Promise<Holiday[]> => {
+  return await fetchHolidays()
+}
+
+export const fetchHolidays = async (): Promise<Holiday[]> => {
+  return await fetch(url)
+    .then((res) => res.json())
+    .then((json: {[key: string]: string}) => {
+      const holidays: Holiday[] = []
+      for (const key in json) {
+        const dt = DateTime.fromFormat(key, 'yyyy-MM-dd')
+          .setZone('Asia/Tokyo')
+        if (!dt.isValid) {
+          continue
+        }
+        holidays.push({
+          date: dt.toJSDate(),
+          text: json[key]
+        })
+      }
+      return holidays.sort((a, b) => +a.date - +b.date)
+    })
+}
+
+export const whatDayIs = async (date: Date): Promise<string | undefined> => {
+  const holidays = await getHolidays()
+  const futureHolidays = holidays.filter((holiday) => compareDate(holiday.date, date))
+  return futureHolidays.length ? futureHolidays[0].text : undefined
+}
+
+export const getHolidaysInThisMonth = async (date: Date): Promise<Holiday[]> => {
+  const holidays = await getHolidays()
+  return holidays.filter((holiday) => dateInMonth(holiday.date, date))
+}
+
+export const getNextHoliday = async (date: Date): Promise<string | undefined> => {
+  const holidays = await getHolidays()
+  const futureHolidays = holidays.filter((holiday) => +holiday.date > +date)
+  return futureHolidays.length ? futureHolidays[0].text : undefined
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,45 @@
 import * as functions from 'firebase-functions'
+import { Client, TextMessage } from '@line/bot-sdk'
+import { DateTime } from 'luxon'
+
+import { getHolidaysInThisMonth, whatDayIs } from '@/holiday'
+import { line } from '@/config'
+
+const client = new Client({
+  channelAccessToken: line.channelAccessToken,
+  channelSecret: line.channelSecret
+})
 
 export const ping = functions.https.onRequest((_, res) => {
   res.send('pong')
 })
 
-// export const pubsub = functions.pubsub
-//   .schedule('34 3 * * *')
-//   .timeZone('Asia/Tokyo')
-//   .onRun(async (_context) => {
-//   })
+export const everyday = functions.pubsub
+  .schedule('0 9 * * *')
+  .timeZone('Asia/Tokyo')
+  .onRun(async (_context) => {
+    const now = DateTime.now()
+      .setZone('Asia/Tokyo')
+
+    const textMessage: TextMessage = {
+      type: 'text',
+      text: `${now.month}月の祝日は`
+    }
+    if (now.day === 1) {
+      const holidaysInThisMonth = await getHolidaysInThisMonth(now.toJSDate())
+      if (holidaysInThisMonth.length) {
+        textMessage.text += `\n${holidaysInThisMonth.map((holiday) => `${DateTime.fromJSDate(holiday.date).setZone('Asia/Tokyo').toFormat('M月d日')} の ${holiday.text}`).join('\n')}\nです`
+      } else {
+        textMessage.text += 'ありません'
+      }
+      client.broadcast(textMessage)
+    } else {
+      const holidayNameToday = await whatDayIs(now.setZone('Asia/Tokyo').plus({ days: 1 }).toJSDate())
+      if (holidayNameToday) {
+        client.broadcast({
+          type: 'text',
+          text: `明日は${holidayNameToday}です`
+        })
+      }
+    }
+  })
